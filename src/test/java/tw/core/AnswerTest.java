15d78c53256e1323d37d863fb0d9c261a5e21b28
quinnHuang <quinn.huang@oocl.com>
2018-07-12 17:05:12 +0800

    [Quinn] test Answer createAnswer

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,6 +2,10 @@ package tw.core;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -13,5 +17,18 @@ public class AnswerTest {
     public void setUp() {
         actualAnswer = Answer.createAnswer("1 2 3 4");
     }
+
+    @Test
+    public void should_create_an_Answer_with_a_number_list_by_createAnswer_when_give_a_String(){
+        //give
+        String input="5 2 8 4";
+
+        //when
+        Answer result=Answer.createAnswer(input);
+
+        //then
+        assertThat(result.toString(),is(input));
+    }
+
 
 }
