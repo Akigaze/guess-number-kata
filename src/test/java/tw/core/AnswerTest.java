53c63490281ad557750c6252a816c6dfcf910fb2
quinnHuang <quinn.huang@oocl.com>
2018-07-12 17:23:30 +0800

    [Quinn] test Answer validate

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -3,9 +3,11 @@ package tw.core;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -21,7 +23,7 @@ public class AnswerTest {
     @Test
     public void should_create_an_Answer_with_a_number_list_by_createAnswer_when_give_a_String(){
         //give
-        String input="5 2 8 4";
+        String input="       5 2 8 4";
 
         //when
         Answer result=Answer.createAnswer(input);
@@ -30,5 +32,41 @@ public class AnswerTest {
         assertThat(result.toString(),is(input));
     }
 
+    @Test
+    public void should_successfully_validate_when_give_a_String_with_right_format(){
+        //give
+        String input="5 2 8 4";
 
+        //when
+        Answer result=Answer.createAnswer(input);
+
+        //then
+        try {
+            result.validate();
+        } catch (AnswerFormatIncorrectException e) {
+            fail("Answer format is incorrect");
+        }
+    }
+
+    @Test
+    public void should_successfully_validate_when_give_a_String_with_wrong_format(){
+        //give
+        String input1="5 111 2 7";
+        String input2="5 1 2 1";
+
+        //when
+        Answer result1=Answer.createAnswer(input1);
+        Answer result2=Answer.createAnswer(input2);
+
+        //then
+        try {
+            result1.validate();
+            fail("Answer format is incorrect");
+        } catch (AnswerFormatIncorrectException e) {
+            try {
+                result2.validate();
+                fail("Answer format is incorrect");
+            } catch (AnswerFormatIncorrectException e1) { }
+        }
+    }
 }
